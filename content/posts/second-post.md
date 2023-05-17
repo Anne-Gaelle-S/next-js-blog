a7faeb9a70c00269bce39fcddce9eaa82f24dacc
Anne-G <ang.schall@gmail.com>
2023-05-17 18:14:00 +0200

    feat: make post page generic to new posts

---
+++ content/posts/second-post.md
@@ -0,0 +1,14 @@
+---
+date: "17/05/2023"
+title: "Second Post"
+---
+
+This is my second post, written in markdown.
+
+Here's some __bold text__.
+
+And a list : 
+
+* One
+* Two
+* Three
---
+++ pages/posts/[post-slug].js
@@ -0,0 +1,45 @@
+// Prod mode : SSG (static side generation HTML + JSON, uses getStaticProps)
+import Head from 'next/head';
+import { getPost } from '../../lib/posts';
+
+// Check if given slug match a valid paths for this route
+export async function getStaticPaths() {
+  console.log('getStaticPaths');
+  return {
+    paths: [
+      { params: { 'post-slug': "first-post" } },
+      { params: { 'post-slug': "second-post" } },
+    ],
+    // dynamic route results in static HTML files being generated because of this fallback :
+    fallback: false, // if none paths above matches the request URL we will show 404 Not Found page
+  };
+}
+
+// Dev mode : SSR
+// Prod mode : pre-render by server (nodejs) and serve a json file for CSR
+export async function getStaticProps(context) {
+  console.log({context});
+  const post = await getPost(context.params['post-slug']);
+  return {
+    props: { post },
+  };
+}
+
+// Dev mode : SSR & CSR
+// Prod mode : CSR
+function PostPage({post}) {
+  return (
+    <>
+      <Head>
+        <title>{`${post.title} - AG's blog`}</title>
+      </Head>
+      <main>
+        <p>{post.date}</p>
+        <h1>{post.title}</h1>
+        <article dangerouslySetInnerHTML={{ __html: post.body }} />
+      </main>
+    </>
+  );
+}
+
+export default PostPage;
