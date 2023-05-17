a9c2cfe8ae0fc25bf90620b331347a832bb5b610
Anne-G <ang.schall@gmail.com>
2023-05-17 14:35:06 +0200

    feat: add navigation to about page

--- pages/index.js
+++ pages/index.js
@@ -1,8 +1,21 @@
+import Link from 'next/link';
+
 function HomePage() {
   console.log('[HomePage] render');
-  return ( <main>
-    <h1>AG's blog</h1>
-  </main> );
+  return (
+    <>
+      <header>
+        <nav>
+          <ul>
+            <li><Link href="/about">About</Link></li>
+          </ul>
+        </nav>
+      </header>
+      <main>
+        <h1>AG's blog</h1>
+      </main>
+    </>
+    );
 }
 
 export default HomePage;
