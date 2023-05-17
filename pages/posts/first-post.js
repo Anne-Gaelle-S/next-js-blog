0a034d6bed1cf0f99768876317586f3a9f637917
Anne-G <ang.schall@gmail.com>
2023-05-17 17:03:57 +0200

    feat: get post content from json file

--- pages/posts/first-post.js
+++ pages/posts/first-post.js
@@ -1,16 +1,14 @@
 // Prod mode : SSG (static side generation HTML + JSON, uses getStaticProps)
+import { readFile } from 'fs/promises';
 import Head from 'next/head';
 
 // Dev mode : SSR
 // Prod mode : pre-render by server (nodejs) and serve a json file for CSR
 export async function getStaticProps() {
+  const jsonContent = await readFile('content/posts/first-post.json', 'utf8');
+  const post = JSON.parse(jsonContent);
   return {
-    props: {
-      post: {
-        title: "First Post",
-        body: "My first post, as static props",
-      },
-    },
+    props: { post },
   };
 }
 
