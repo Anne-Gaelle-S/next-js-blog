8161a82ccfe46c76cc57bf4a0c002b3639ccd296
Anne-G <ang.schall@gmail.com>
2023-05-22 10:07:01 +0200

    feat: add toggle button for darkMode

---
+++ components/ThemeSwitch.js
@@ -0,0 +1,23 @@
+import { useState } from "react";
+
+function ThemeSwitch() {
+  const [darkMode, setDarkMode] = useState(false);
+
+  const text = darkMode ? 'Light Mode' : 'Dark Mode';
+  return (
+    <>
+      <button onClick={() => setDarkMode(!darkMode)}>
+        {text}
+      </button>
+      <style jsx>{`
+        button {
+          background: none;
+          border: none;
+          cursor: pointer;
+        }
+      `}</style>
+    </>
+  );
+}
+
+export default ThemeSwitch;
