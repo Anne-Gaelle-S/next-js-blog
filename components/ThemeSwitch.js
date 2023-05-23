1d5581bbfab61cb938bcc1bed971ea344f406b4b
Anne-Gaelle-S <ang.schall@gmail.com>
2023-05-23 16:56:43 +0200

    feat: load darkmode from localstorage

--- components/ThemeSwitch.js
+++ components/ThemeSwitch.js
@@ -1,8 +1,24 @@
 import { useState } from "react";
 import DarkTheme from "./DarkTheme";
 
+function loadDarkMode() {
+  const isOnServerSideRendering = typeof window === 'undefined';
+  if (isOnServerSideRendering) {
+    return false;
+  }
+  const value = localStorage.getItem('darkMode');
+  return value === null ? false : JSON.parse(value);;
+}
+
 function ThemeSwitch() {
-  const [darkMode, setDarkMode] = useState(false);
+  const [darkMode, setDarkMode] = useState(loadDarkMode);
+
+  // An alternative to loadDarkMode function :
+  // useEffect(() => {
+  //   const value = localStorage.getItem('darkMode');
+  //   const result = value === null ? false : JSON.parse(value);
+  //   setDarkMode(result);
+  // }, []);
 
   const handleClick = () => {
     localStorage.setItem('darkMode', JSON.stringify(!darkMode));
@@ -12,7 +28,7 @@ function ThemeSwitch() {
   const text = darkMode ? 'Light Mode' : 'Dark Mode';
   return (
     <>
-      <button onClick={handleClick}>
+      <button onClick={handleClick} suppressHydrationWarning>
         {text}
       </button>
       <style jsx>{`
